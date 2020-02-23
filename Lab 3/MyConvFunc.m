802edc3d9f4c2c3e78e5474d5ccd24cc545384a4
Zak (Lenovo) <zakcatherall@outlook.com>
2020-02-23 14:22:26 +0000

    got my conv closer to inbuilt oen

--- Lab 3/MyConvFunc.m
+++ Lab 3/MyConvFunc.m
@@ -1,66 +1,120 @@
+% @TODO: Padding param is currently redundant because new img array is prepopulated with 0s.
+% @TODO: Window and filter kernels are currently hard coded to be 3x3.
+% @TODO: Stride is hard coded.
+
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+% PROGRAM CODE %
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+
+% Load image; resize for quicker testing.
 cece = imread("Celica.jpg");
 % cece = imresize(cece, 0.5);
-% myFilter = [0 1 0; 1 -4 -1; 0 -1 0];
+
+
+%  Generate filter/s.
+myFilter = [0 1 0; 1 -4 -1; 0 -1 0];
 % myFilter = [0 1 0; 0 -2 0; 0 -1 0];
-myFilter1 = [0 0 0; 0 -1 0; 0 1 0];
-myFilter2 = [0 0 0; 0 -1 1; 0 0 0];
-% myFilter = myFilter'; % transpose
+% myFilter1 = [0 0 0; 0 -1 0; 0 1 0];
 
-convImg1 = MyConv(cece,myFilter1, 0);
-% convImg2 = MyConv(cece,myFilter2, 0);
-convImg2 = convn(cece,myFilter1, 'same');
 
+% Manipulate image/s.
+convImg1 = convn(cece,myFilter, 'same');
+convImg2 = MyConv(cece,myFilter, 255);
+
+
+% With different colour models...
+ceceHSV = rgb2hsv(cece);
+ceceHSV = MyConv(ceceHSV,myFilter, 255);
+ceceLAB = rgb2lab(cece);
+ceceLAB = MyConv(ceceLAB,myFilter, 255);
+ceceYCBCR = rgb2ycbcr(cece);
+ceceYCBCR = MyConv(ceceYCBCR,myFilter, 255);
+
+% Plot image/s.
 figure
-subplot(1,2,1);
+subplot(2,3,1);
+imshow(cece)
+title("Cece <3");
+subplot(2,3,2);
 imshow(convImg1)
-subplot(1,2,2);
+title("MatLab convn()");
+subplot(2,3,3);
 imshow(convImg2)
-% 
-% 
-% 
-% @TODO: PADDING IS REDUNDANT WITH THIS IMPLEMENTATION 
-% 
-%  FACADE
+title("MyConv()");
+subplot(2,3,4);
+imshow(ceceHSV)
+title("MyConv() HSV");
+subplot(2,3,5);
+imshow(ceceLAB)
+title("MyConv() L*A*B");
+subplot(2,3,6);
+imshow(ceceYCBCR)
+title("MyConv() YcBcR");
+
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+% METHODS %
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+
+%  FACADE METHOD
+% Traverse the image with a "sliding window" and convolve the image.
 function convImg = MyConv(img, myFilt, padding)
-    convImg = img; % Make a copy.
-    [x,y,z] = size(img);
-    for ch = 1:z
-        for col = 1:y
-            for row = 1:x
+    % @TODO: ASK ASHLEY WHY NEXT LINE DOES SOME NIIICE EDGE DETECTION.
+    % convImg = img; % Make a copy to modify and return.
+
+    [x,y,z] = size(img); % Get iterators as img dimensions.
+    convImg = zeros(y,x,z); % Initialise empty output for speed.
+
+    % @TODO: order of dimensions doesn't SEEM to have any effect.
+    for ch = 1:z % Step through channels.
+        for col = 1:y % Step through rows.
+            for row = 1:x % Step through columns.
+                % Get "window".
                 neighbours = GetNeighbours(img, row, col, ch, padding);
+
+                % Convolve the "window" with the filter.
                 newPixel = Filt(neighbours, myFilt);
+
+                % Set the value in the output/return img.
                 convImg(row,col,ch) = newPixel;
             end
         end
     end
 end
+ 
 
-% IMPLEMENTATION
+% IMPLEMENTATION METHOD
+% Returns the "window" of neighbours.
 function neighbours = GetNeighbours(img, row, col, ch, padding)
-    nArray = [-1 0 1];
-    neighbours = zeros(3,3);
+    nArray = [-1 0 1]; % Neighbour positions i.e. left/up, current x/y, right/down.
+    neighbours = zeros(3,3); % 3x3 kernel.
 
-    for ny = [1 2 3]
-        for nx = [1 2 3]
-            y = row + nArray(ny);
-            x = col + nArray(nx); 
+    for ny = [1 2 3] % To step through nArray.
+        for nx = [1 2 3]  % To step through nArray.
+            y = row + nArray(ny); % Get y val for this neighbour.
+            x = col + nArray(nx); % Get x val for this neighbour.
             try
+                % Get the value of the neighbour pixel from the actual image.
                 pixel = img(y,x,ch);
             catch
+                % If neighbour doesn't exist, use the padding value.
                 pixel = padding; 
             end
             
+            % Return the populated window.
             neighbours(ny, nx) = pixel;
         end
     end
 end
 
-% IMPLEMENTATION
+
+% IMPLEMENTATION METHOD
+% Convolve a window of the image with a filter.
 function scalar = Filt(neighbours, myFilt)
     % Get each pixel value left-right, top-down - of the window/neighbours.
     scalar = 0;
-    for y = [1 2 3]
-        for x = [1 2 3]
+    for y = [1 2 3] % Step through rows of neighbours and filter.
+        for x = [1 2 3] % Step through cols of neighbours and filter.
+            % Calculate runnning total (double sum).
             scalar = scalar + (neighbours(y,x) * myFilt(y,x));
         end
     end
